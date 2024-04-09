cc80af4beea76aaabe109f2125007ae730e61f16
darwgom7 <darwgom7@gmail.com>
2024-04-08 21:45:17 -0500

    Initial commit

---
+++ src/main/java/com/darwgom/candidatecontrolapi/CandidatecontrolApiApplication.java
@@ -0,0 +1,13 @@
+package com.darwgom.candidatecontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidatecontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidatecontrolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darwgom/candidatecontrolapi/CandidatecontrolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwgom.candidatecontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidatecontrolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
